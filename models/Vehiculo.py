45a7bba73fda259452677b5d334a4a7c89f7e67d
Marco Antonio Montoya Morales <mamm1990@gmail.com>
2021-02-13 15:55:28 -0500

    Ajuste Modelo - Persistencia

--- models/Vehiculo.py
+++ models/Vehiculo.py
@@ -9,15 +9,13 @@ class Vehiculo(db.Base):
     id_usuario = Column('id_usuario', String(60), nullable=False)
     tipo_vehiculo = Column('tipo_vehiculo', String(100), nullable=False)
     placa = Column('placa', String(10), nullable=False)
-    estado_vehiculo = Column('estado_vehiculo', String(30), nullable=False)
     fecha_crea = Column('fecha_crea', DateTime, server_default=func.now(), nullable=True)
  
-    def __init__(self, id_vehiculo, id_usuario, tipo_vehiculo, placa, estado_vehiculo):
+    def __init__(self, id_vehiculo, id_usuario, tipo_vehiculo, placa):
         self.id_vehiculo = id_vehiculo
         self.id_usuario = id_usuario
         self.tipo_vehiculo = tipo_vehiculo
         self.placa = placa
-        self.estado_vehiculo = id_usuario
  
     def __repr__(self):
         return f"<Vehiculo {self.id_vehiculo}>"
--- models/Zona.py
+++ models/Zona.py
@@ -7,13 +7,10 @@ class Zona(db.Base):
  
     id_zona = Column('id_zona', String(5), primary_key=True, nullable=False)
     disponible = Column('disponible', Boolean, nullable=False)
-    estado_zona = Column('estado_zona', String(30), nullable=False)
     fecha_crea = Column('fecha_crea', DateTime, server_default=func.now(), nullable=True)
  
-    def __init__(self, id_zona, disponible, estado_zona):
+    def __init__(self, id_zona, disponible):
         self.id_zona = id_zona
-        self.disponible = disponible
-        self.estado_zona = estado_zona
- 
+        self.disponible = disponible 
     def __repr__(self):
         return f"<Zona {self.id_zona}>"
